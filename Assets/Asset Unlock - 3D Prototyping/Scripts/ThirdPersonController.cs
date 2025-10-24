a0591e5e39c9f0b0b1cb72d534e4e4bda724ffd5
msanchezru <msanchezru@edu.tecnocampus.cat>
2025-10-24 17:12:23 +0200

    FPSController+Weapon + Inici shader

--- Assets/Asset Unlock - 3D Prototyping/Scripts/ThirdPersonController.cs
+++ Assets/Asset Unlock - 3D Prototyping/Scripts/ThirdPersonController.cs
@@ -2,9 +2,173 @@
 using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonController : MonoBehaviour
 {
+
+    [Header("References")]
+    [SerializeField] private Transform MPitchController;
+    [SerializeField] private CharacterController _mCharacterController;
+
+    [Header("Movement")]
+    public float maxSpeedOnGround = 10;
+    public float maxSpeedRunning = 20;
+    private Vector2 mDirection = Vector2.zero;
+    private bool mIsRunning = false;
+
+    [Header("Jumping")]
+    public float jumpSpeed = 10;
+    private float mVerticalSpeed = 0;
+    public bool IsGrounded = false;
+
+    [Header("Rotation")]
+    public int rotationSpeed = 10;
+    public bool invertPitch = false;
+
+    [Header("Camera Settings")]
+    [SerializeField] private Camera playerCamera;
+    public float normalFOV = 60;
+    public float sprintFOV = 75;
+
+    [Header("Weapon")]
+    [SerializeField] private Weapon weapon;
+    private bool isShooting = false;
+
+    private bool isRespawning = false;
+
+    [Range(-89, 89)] public float minPitch = -89;
+    [Range(-89, 89)] public float maxPitch = 89;
+
+    private float currentPitch = 0;
+    private float currentYaw = 0;
+
+    void OnEnable()
+    {
+        //GameManager.OnPlayerRespawn += HandlePlayerRespawn; //per desactivar el movement un moment
+        //GameManager.OnGameRestart += HandlePlayerRespawn;
+        //GameManager.OnPlayerDeath += HandlePlayerRespawn;
+    }
+
+    void Start()
+    {
+        currentPitch = MPitchController.localRotation.eulerAngles.x;
+        currentYaw = MPitchController.localRotation.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        //if (isRespawning) return;
+
+
+        //A l'update millor modificar només paràmetres, fer els càlculs a part.
+        //Del profe: existeix Vector2 pos = Input.mousePositionDelta; per no haver de guardar la posició del frame anterior
+        
+        // ROTATION
+        float pitch = Input.GetAxis("Mouse Y") * rotationSpeed * (invertPitch ? 1 : -1);
+        //deltaMove.y * rotationSpeed * time.deltaTime per fer-ho frame independent
+        currentPitch = Mathf.Clamp(currentPitch + pitch, minPitch, maxPitch);
+        
+        //float yaw = Input.GetAxis("Mouse X") * rotationSpeed*2 * Time.deltaTime;
+        float yaw = Input.GetAxis("Mouse X") * rotationSpeed;
+        currentYaw += yaw;
+
+        MPitchController.localRotation = Quaternion.Euler(currentPitch, 0, 0);
+        //localRotation seria només al component, rotation és respecte al món. En aquest cas funcionen els dos. Millor rotation.
+        transform.rotation = Quaternion.Euler(0, currentYaw, 0); //Aquí és local perquè el yaw afecta al cos sencer del Player, no només a la càmera.
+
+        // RUNNING
+        float speed = mIsRunning ? maxSpeedRunning : maxSpeedOnGround;
+
+        // Speed FOV
+        //Quan estigui corrent, ampliar el camp de visió (FOV) de la càmera per donar sensació de velocitat
+        float targetFOV = mIsRunning ? sprintFOV : normalFOV;
+        //També es podria fer amb una interpolació lineal Lerp perquè sigui més suau el canvi -> A+(B-A)*t
+        playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, targetFOV, Time.deltaTime * 10); //el 10 és la velocitat d'interpolació
+
+        // MOVEMENT
+        Vector3 movement = (transform.forward * mDirection.y + transform.right * mDirection.x) * speed * Time.deltaTime;
+        _mCharacterController.Move(movement);
+
+        // JUMP
+        //MRUA: y = y0 + v0*t + 0.5*a*t^2    (Physics.gravity millor que posar -9.81)
+        mVerticalSpeed += Physics.gravity.y * Time.deltaTime; //la velocitat amb el temps decreix i amb el temps arriba a ser 0 (i podria ser negativa)
+        Vector3 verticalMovement = Vector3.up * mVerticalSpeed * Time.deltaTime;
+        CollisionFlags collisionFlags = _mCharacterController.Move(verticalMovement);
+
+        //IsGrounded = _mCharacterController.isGrounded; //Falla més que el collisionFlags
+        IsGrounded = (collisionFlags & CollisionFlags.Below) != 0; //El & binari és per comprovar flags en bitmask
+        if (IsGrounded && mVerticalSpeed > 0.0f)
+            mVerticalSpeed = 0;
+
+        // SHOOT
+        /*
+        if (isShooting && weapon != null)
+        {
+            weapon.Shoot();
+        }
+        */
+    }
+
+    /*
+    public void OnTriggerEnter(Collider other)
+    {
+        Item item = other.GetComponent<Item>();
+        if (item != null)
+        {
+            item.Pick(this.gameObject);
+        }
+    }*/
+
+
+    /*
+    Input Value: Per moviment continu, rotació de càmera, controls analògics
+    CallbackContext: Per accions discretes com saltar, disparar, interactuar, canviar armes
+
+    No es poden fer servir els dos alhora perquè InputValue necessita que el PlayerInput sigui Send Messages i CallbackContext necessita que sigui InvokeEvents.
+    */
+    public void OnMove(InputAction.CallbackContext context){
+        mDirection = context.ReadValue<Vector2>();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context){
+        mIsRunning = context.performed; //started quan es prem, performed quan es manté premut i canceled quan es deixa de prémer
+    }
+
+    public void OnJump(InputAction.CallbackContext context){
+        if (!IsGrounded || !context.performed) return;
+        mVerticalSpeed = jumpSpeed;
+        IsGrounded = false;
+    }
+
+    public void OnShoot(InputAction.CallbackContext context){
+        isShooting = context.performed && weapon != null;
+    }
+
+    public void OnReload(InputAction.CallbackContext context){
+        //if (context.started && weapon != null)
+        //    weapon.Reload();
+    }
+
+    public Weapon GetWeapon()
+    {
+        return weapon;
+    }
+
+    private void HandlePlayerRespawn()
+    {
+        isRespawning = true;
+        StartCoroutine(ResetRespawnVariable());
+    }
+
+    private IEnumerator ResetRespawnVariable()
+    {
+        yield return new WaitForSeconds(1f);
+        isRespawning = false;
+    }
+
+
+    /*
     public Camera GameCamera;
     public float playerSpeed = 2.0f;
     private float JumpForce = 1.0f;
@@ -62,4 +226,5 @@ public class ThirdPersonController : MonoBehaviour
 
         m_Controller.Move(playerVelocity * Time.deltaTime);
     }
+    */
 }
