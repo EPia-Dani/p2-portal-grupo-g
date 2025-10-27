7ff16d926ace0c7e7ecf231368190c9c11f35992
msanchezru <msanchezru@edu.tecnocampus.cat>
2025-10-27 16:56:48 +0100

    Funciona un portal, l'altre no

--- Assets/_Scripts/Portal.cs
+++ Assets/_Scripts/Portal.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Camera playerCamera;
-    public Transform reflectionPlane;
+    public Transform reflectionTransform;
     public Camera reflectionCamera;
     public Portal mirrorPortal;
     public float offsetNearPlane;
@@ -13,17 +13,17 @@ public class Portal : MonoBehaviour
         playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera").GetComponent<Camera>();
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         Vector3 worldPosition = playerCamera.transform.position;
-        Vector3 localPosition = transform.InverseTransformPoint(worldPosition);
+        Vector3 localPosition = reflectionTransform.InverseTransformPoint(worldPosition);
         mirrorPortal.reflectionCamera.transform.position = mirrorPortal.transform.TransformPoint(localPosition);
 
         Vector3 worldDirection = playerCamera.transform.forward;
-        Vector3 localDirection = transform.InverseTransformDirection(worldDirection);
+        Vector3 localDirection = reflectionTransform.InverseTransformDirection(worldDirection);
         mirrorPortal.reflectionCamera.transform.forward = mirrorPortal.transform.TransformDirection(localDirection);
 
         float distance = Vector3.Distance(mirrorPortal.reflectionCamera.transform.position, mirrorPortal.transform.position);
-        mirrorPortal.reflectionCamera.nearClipPlane = distance + offsetNearPlane;
+        mirrorPortal.reflectionCamera.nearClipPlane = Mathf.Max(0.0f, distance) + offsetNearPlane;
     }
 }
